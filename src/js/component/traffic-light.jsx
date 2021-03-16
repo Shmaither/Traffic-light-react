a68842df5affd1ea0a2d762a70f8793ef340cf9e
Shmaither M. Rizo <shmaither@gmail.com>
2021-03-16 03:34:50 +0000

    TL (feature): reacting to click event

--- src/js/component/traffic-light.jsx
+++ src/js/component/traffic-light.jsx
@@ -4,23 +4,36 @@ import React, { useState } from "react";
 
 //create your first component
 export function TrafficLight() {
-	const [colorLight, setColor] = useState("red");
+	const [colorLight, setColor] = useState(null);
 
-	console.log(colorLight);
+	let redGlow = "";
+	let yellowGlow = "";
+	let greenGlow = "";
+
+	function fAssignedGlow(color) {
+		setColor(color);
+
+		redGlow = colorLight === "red" ? "glow" : "";
+		console.log(redGlow);
+		yellowGlow = colorLight === "yellow" ? "glow" : "";
+		console.log(yellowGlow);
+		greenGlow = colorLight === "green" ? "glow" : "";
+		console.log(greenGlow);
+	}
 
 	return (
 		<>
 			<div className="pole"></div>
 			<div className="blackBox">
 				<div
-					className="circle red"
-					onClick={() => setColor("red")}></div>
+					className={"circle red " + redGlow}
+					onClick={() => fAssignedGlow("red")}></div>
 				<div
-					className="circle yellow"
-					onClick={() => setColor("yellow")}></div>
+					className={"circle yellow " + yellowGlow}
+					onClick={() => fAssignedGlow("yellow")}></div>
 				<div
-					className="circle green"
-					onClick={() => setColor(() => setColor("green"))}></div>
+					className={"circle green " + greenGlow}
+					onClick={() => fAssignedGlow("green")}></div>
 			</div>
 		</>
 	);
